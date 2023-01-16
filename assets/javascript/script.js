effe35ae6ca7de87d4e7aad778a0a5c31b80d5bf
cameronmcnz~clear <ashly_mossette@yahoo.com>
2023-01-16 13:19:24 -0800

    quick save - modified index.html, and css skeleton

---
+++ assets/javascript/script.js
@@ -0,0 +1,18 @@
+//DOM elements
+
+// Event listerner to start quiz 
+
+
+//Var Questions and answers 
+
+
+//if else conditional statements 
+
+
+
+//For loops 
+
+
+//local storage
+
+//ending event listner?? 
