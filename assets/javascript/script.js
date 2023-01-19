e88944dc5bf5c6b83d57c94885506943303b79c3
cameronmcnz~clear <ashly_mossette@yahoo.com>
2023-01-18 23:05:24 -0800

    puesdocoded javascript and updated html

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -5,6 +5,68 @@
 
 //Var Questions and answers 
 
+var questions = [
+    {
+        question: 'What are the data types supported by Javascript?'
+        answer: [
+            {text: 'Undefined, Null', correct: false}
+            {text: 'Boolean, String', correct: false}
+            {text: 'Symbol, Number, Object', correct: false}
+            {text: 'All of the above', correct: true}
+            {text: 'None of the above', correct: false}
+        ]
+        
+    },
+    {
+        question: 'How can you create an Array in Javascript?'
+        answer: [
+            {text: 'var y = [1, 2, 3, 4, 5];', correct: true}
+            {text: 'var y = {1, 2, 3, 4, 5};', correct: false}
+            {text: 'Both A & B', correct: false}
+            {text: 'None of the above', correct: false}
+        ]
+    },
+    {
+        question: 'Which is the assignment operator?'
+        answer: [
+            {text: '/', correct: false}
+            {text: ':', correct: false}
+            {text: ';', correct: false}
+            {text: '=', correct: true}
+        ]
+    },
+    {
+        question: 'How would you execute a callback?'
+        answer: [
+            {text: 'function[]', correct: false}
+            {text: '.function', correct: false}
+            {text: 'function()', correct: true}
+            {text: 'All of the Above', correct: false}
+            {text: 'None of the Above', correct: false}
+        ]
+    },
+    {
+        question: '== and === are ____?'
+        answer: [
+            {text: 'methods', correct: false}
+            {text: 'functions', correct: false}
+            {text: 'scopes', correct: false}
+            {text: 'operators', correct: true}
+            {text: 'booleans', correct: false}
+        ]
+    },
+    {
+        question: 'What method is used to output a message to the web, and is typically used for debugging? '
+        answer: [
+            {text: 'console.log', correct: true}
+            {text: 'method.log', correct: false}
+            {text: 'console.log()', correct: false}
+            {text: 'method.log()', correct: false}
+            {text: 'debug.log{}', correct: false}
+        ]
+    }
+]
+
 
 //if else conditional statements 
 
